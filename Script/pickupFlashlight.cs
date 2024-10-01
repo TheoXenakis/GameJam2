21000bf017350542fbd024ca1a8bc5fe60aeaf2c
F00L0 <124820432+F00L0@users.noreply.github.com>
2024-09-30 19:34:20 -0700

    Add files via upload

--- Script/pickupFlashlight.cs
+++ Script/pickupFlashlight.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class pickupFlashlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject inttext, flashlight_table, flashlight_hand;
+    public AudioSource pickup;
+    public bool interactable;
 
-    // Update is called once per frame
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            inttext.SetActive(true);
+            interactable = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            inttext.SetActive(false);
+            interactable = false;
+        }
+    }
     void Update()
     {
-        
+        if(interactable == true)
+        {
+            //pickupflashlight with control E
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                inttext.SetActive(false);
+                interactable = false;
+                //pickup.Play();
+                flashlight_hand.SetActive(true);
+                flashlight_table.SetActive(false);
+            }
+        }
     }
 }
+
